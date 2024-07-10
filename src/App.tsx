bbb4f1468668e5b18ec19837ee73aad57dbda845
ProgrammerShahJalal <you@example.com>
2024-07-10 08:52:24 +0600

    project setup redux shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Mechanical Keyboard</h1>
+      <Button>My Button</Button>
+    </>
+  );
+}
+
+export default App;
